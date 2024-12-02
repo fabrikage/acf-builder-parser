4167ab240cf6fa7417928c961ca1f7160916a0fd
Bart Klein Reesink <bartkleinreesink@gmail.com>
2024-12-02 18:00:02 +0100

    Not yet ready

--- src/Export/Fields/Types/TypeFlexibleContent.php
+++ src/Export/Fields/Types/TypeFlexibleContent.php
@@ -2,12 +2,8 @@
 
 namespace Fabrikage\AcfBuilderParser\Export\Fields\Types;
 
-use Fabrikage\AcfBuilderParser\Export\Fields\Traits\HasLayouts;
-
 final class TypeFlexibleContent extends TypeAbstract
 {
-    use HasLayouts;
-
     public static function getTypeName(): string
     {
         return 'flexible_content';
